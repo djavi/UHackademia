9f8875a346231bbefed4c293d7ffe15d1c9f0988
Joe Catarata <joecatarata@gmail.com>
2018-06-29 23:31:45 +0000

    Migrated models.py

--- uhack/ecobrick/models.py
+++ uhack/ecobrick/models.py
@@ -11,32 +11,30 @@ class UserDetail(models.Model):
     lastName = models.CharField(max_length = 50)
     address = models.CharField(max_length = 150)
     contactNum = models.CharField(max_length = 50)
-    
+
     #user account type
     choices = ((0, 'Staff'), (1, 'Regular User'))
     userType = models.IntegerField(default = 1, choices = choices)
-    
-        
+
+
 class Brick(models.Model):
     #brick details
     user = models.ForeignKey(User,on_delete = models.CASCADE)
     brickWeight = models.PositiveIntegerField(default = 0)
     brickVolume = models.PositiveIntegerField(default = 0)
     brickPoint = models.PositiveIntegerField(default = 0)
-    
+
 class Reward(models.Model):
     #reward details
     rewardName = models.CharField(max_length = 250)
     pointCost = models.PositiveIntegerField(default = 0)
     description = models.CharField(max_length = 1000)
-    
+
 class Partner(models.Model):
     #partner details
     region = models.CharField(max_length = 100)
     city = models.CharField(max_length = 100)
     barangay = models.CharField(max_length = 100)
-    lat = models.DecimalField(default = 0)
-    long = models.DecimalField(default = 0)
+    lat = models.DecimalField(default = 0, decimal_places = 10, max_digits=15)
+    long = models.DecimalField(default = 0, decimal_places = 10, max_digits=15)
     contactNum = models.CharField(max_length = 50)
-    
-    
