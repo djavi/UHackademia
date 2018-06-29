ae864098445ae9747982da7bd1c2485004299645
ejcaguiat <joshuacaguiat@yahoo.com>
2018-06-30 00:35:44 +0800

    asdf

--- uhack/ecobrick/urls.py
+++ uhack/ecobrick/urls.py
@@ -3,9 +3,6 @@ from . import views
 import django.views.defaults
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),
-<<<<<<< HEAD
-    path('staff/',views.staff_view,name="staff")
-=======
+    path('staff/',views.staff_view,name="staff"),
     path('login/', views.login_view, name="login"),
->>>>>>> 2904ed9c253d683de246e709081e0d8b76511a96
 ]
